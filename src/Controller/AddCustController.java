1be1ca7c773d885da89f3857d6507b9093ce5aab
Meenan0811 <meenan@mac.com>
2023-06-22 11:04:56 -0600

    Employee table on Mainwin is populating, added action to repair and employee buttons. Added deleteRepair method to Alerts

--- src/Controller/AddCustController.java
+++ src/Controller/AddCustController.java
@@ -1,11 +1,15 @@
 package Controller;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
-public class AddCustController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AddCustController implements Initializable {
     @FXML
     private TextField custNameText;
     @FXML
@@ -25,4 +29,12 @@ public class AddCustController {
     @FXML
     private Label addCustLabel;
 
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        if (MainWinController.passCust != null) {
+            custNameText.setText(MainWinController.passCust.getCustName());
+            custPhoneText.setText(MainWinController.passCust.getCustPhone());
+        }
+    }
 }
